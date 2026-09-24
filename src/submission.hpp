38396248c8bb33e43e6480ae9a40710a1b9b9f7b
Ayaanator <ayaan.adrito@gmail.com>
2026-09-24 00:00:37 -0400

    optimize row col retrieval

--- src/submission.hpp
+++ src/submission.hpp
@@ -39,21 +39,24 @@ public:
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid. Implement your solution here.
 void apply_stencil(const Grid& old_grid, Grid& new_grid) {
-  for(std::size_t i = 1; i + 1 < old_grid.get_rows(); i++) {
-    for(std::size_t j = 1; j  + 1 < old_grid.get_cols(); j++) {
+  std::size_t rows = old_grid.get_rows();
+  std::size_t cols = old_grid.get_cols();
+
+  for(std::size_t i = 1; i + 1 < rows; i++) {
+    for(std::size_t j = 1; j  + 1 < cols; j++) {
       new_grid(i, j) = 0.5 * old_grid(i, j) +
         0.125 * (old_grid(i - 1, j) + old_grid(i + 1, j) +
           old_grid(i, j - 1) + old_grid(i, j + 1));
     }
   }
 
-  for (std::size_t j = 0; j < old_grid.get_cols(); j++) {
+  for (std::size_t j = 0; j < cols; j++) {
     new_grid(0, j) = old_grid(0, j);
-    new_grid(old_grid.get_rows() - 1, j) = old_grid(old_grid.get_rows() - 1, j);
+    new_grid(rows - 1, j) = old_grid(rows - 1, j);
   }
 
-  for (std::size_t i = 0; i < old_grid.get_rows(); i++) {
+  for (std::size_t i = 0; i < rows; i++) {
     new_grid(i, 0) = old_grid(i, 0);
-    new_grid(i, old_grid.get_cols() - 1) = old_grid(i, old_grid.get_cols() - 1);
+    new_grid(i, cols - 1) = old_grid(i, cols - 1);
   }
 }
